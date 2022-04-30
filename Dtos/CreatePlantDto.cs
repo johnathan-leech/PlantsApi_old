20f260a2828d6677d678b0034c07b37825286cd3
Johnathan Leech <email@jleech.io>
2022-04-29 20:37:26 -0400

    add data annotations to DTO so that invalid data cannot be added

--- Dtos/CreatePlantDto.cs
+++ Dtos/CreatePlantDto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PlantCatalog.Dtos
 {
     public record CreatePlantDto
     {
+        [Required]
         public string Name { get; init; }
+        [Required]
+        [Range(1, 12)]
         public int MinZone { get; init; }
+        [Required]
+        [Range(1, 12)]
         public int MaxZone { get; init; }
     }
 
