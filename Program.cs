94fbd8e024f88140727455104947b3aad040051b
Johnathan Leech <email@jleech.io>
2022-04-30 00:30:04 -0400

    add health checks to configuration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Net.Mime;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -11,12 +14,12 @@ var builder = WebApplication.CreateBuilder(args);
 // when Guid and DateTimeOffset datatyped objects are stored in mongoDb, do so as String datatype.
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+var mongoDbConfig = builder.Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
 
 // Add services to the container.
 builder.Services.AddSingleton<IMongoClient>(serviceProvider => 
 {
-    var config = builder.Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
-    return new MongoClient(config.ConnectionString);
+    return new MongoClient(mongoDbConfig.ConnectionString);
 });
 
 builder.Services.AddSingleton<IPlantsRepository, MongoDbPlantsRepository>();
@@ -27,6 +30,13 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddMongoDb(
+        mongoDbConfig.ConnectionString, 
+        name: "mongodb", 
+        timeout: TimeSpan.FromSeconds(5),
+        tags: new[] { "ready" }
+);
 
 var app = builder.Build();
 
@@ -38,9 +48,30 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+// check the health status of the service (is it running)
+app.MapHealthChecks("/health/live", new HealthCheckOptions{ Predicate = (_) => false });
+// check the health status of the database
+app.MapHealthChecks("/health/ready", new HealthCheckOptions{ 
+    Predicate = (check) => check.Tags.Contains("ready"),
+    ResponseWriter = async(context, report) => {
+        var result = JsonSerializer.Serialize(
+            new{
+                status = report.Status.ToString(),
+                checks = report.Entries.Select(entry => new{
+                    name = entry.Key,
+                    status = entry.Value.Status.ToString(),
+                    duration = entry.Value.Duration.ToString(),
+                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                    description = entry.Value.Description != null ? entry.Value.Description : "none"
+                })
+            }
+        );
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
+});
 
 app.Run();
